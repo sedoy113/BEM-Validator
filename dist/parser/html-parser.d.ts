f23e62c5e6d3c88967d36f7a7543251f610b219b
sedoy113 <sedoy113@yandex.ru>
2026-03-09 14:17:54 +0300

    del dist

--- dist/parser/html-parser.d.ts
+++
@@ -1,9 +0,0 @@
-import type { HtmlElement } from '../types.js';
-/**
- * Парсит HTML и извлекает элементы с классами
- */
-export declare function parseHtml(html: string, fileName?: string): HtmlElement[];
-/**
- * Извлекает все классы из HTML
- */
-export declare function extractClasses(html: string): string[];
